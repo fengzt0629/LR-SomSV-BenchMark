0a581e0127ca6b5623c2b3afb69ecf9b82620ee0
fengzt0629 <117270859+fengzt0629@users.noreply.github.com>
2025-07-28 15:16:20 +0800

    Add files via upload
    SVision-pro

---
+++ 01.Somatic_SV_Find/config.py
@@ -0,0 +1,33 @@
+import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from multiprocessing import Pool
+
+samtools = "samtools" # Version 1.9
+bedtools = "bedtools" # Version 2.30.0
+sniffles2 = "sniffles" #Version 2.0.6
+minimap2 = 'minimap2' #Version 2.17
+seqkit = 'seqkit' #Version 2.4.0
+tr_bed = "human_GRCh38_no_alt_analysis_set.trf.bed" #from sniffels
+
+
+ref_fasta = 'hg38_fa'
+sampleid = 'Sample'
+ref_name = 'hg38_fa'
+tumor_fastq = 'tumor_fastq'
+blood_fastq = 'blood_fastq'
+model_path = "model_liteunet_256_8_16_32_32_32.pth"
+tag = "sampleid"
+
+
+work_dir = 'work_dir'
+
+data_dir = os.path.join(work_dir, '01.Golden.Standard')
+program_dir = os.path.join(work_dir, '02.program')
+ms_dir = os.path.join(data_dir, 'minimap2_sniffles')
+coverage_dir = os.path.join(data_dir,'coverage')
+somatic_dir = os.path.join(data_dir, "Somatic_minimap2_2.4"+ref_name)
+
+
+
